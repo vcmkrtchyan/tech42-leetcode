22b5605088287af7d7364a5c317c431bf0c5fad0
tigranha <76497804+tigranha@users.noreply.github.com>
2021-04-08 15:42:48 +0400

    Update L167.java

--- Tigran Hakobyan/L167.java
+++ Tigran Hakobyan/L167.java
@@ -34,3 +34,23 @@ class Solution {
      return resault;   
     }
 }
+
+// My best solution
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+ 
+            int[] resault = new int[2];
+            for(int i = 0,j = numbers.length-1; i<j;){
+           if(numbers[i] + numbers[j] < target){
+               i++;
+           }else if (numbers[i] + numbers[j]> target){
+               j--;
+           }else{
+               resault[0] = i+1;
+               resault[1] = j+1;
+               break;
+            }
+        }
+       return resault;
+    }
+}

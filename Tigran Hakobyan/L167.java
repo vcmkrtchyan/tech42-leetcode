bcc353c581e9739f4dd48f753eceb503ea846213
tigranha <76497804+tigranha@users.noreply.github.com>
2021-04-08 14:41:08 +0400

    Update L167.java

--- Tigran Hakobyan/L167.java
+++ Tigran Hakobyan/L167.java
@@ -14,3 +14,23 @@ class Solution {
      return resault;   
     }
 }
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] resault = new int[2];
+        
+        for(int i = 1, j = 0; i<numbers.length;i++){
+            if(numbers[j] + numbers[i] == target){
+                resault[0] = j +1;
+                resault[1] = i + 1;
+                break;
+            }
+            if (i == numbers.length-1){
+                j ++;
+                i = j;
+            }
+            
+        }
+     return resault;   
+    }
+}

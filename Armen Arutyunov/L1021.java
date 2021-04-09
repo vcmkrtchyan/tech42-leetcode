c1d59b3e1ad2de8dcb200e0820dd97cbcb1ea4ca
Armen Harutyunov <68850624+Armen82@users.noreply.github.com>
2021-04-09 23:50:26 +0400

    Update L1021.java

---
+++ Armen Arutyunov/L1021.java
@@ -0,0 +1,15 @@
+public class L1021 {
+    public String removeOuterParentheses(String S) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(' && count++ > 0) {
+                sb.append(c);
+            }
+            if (c == ')' && count-- > 1) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}

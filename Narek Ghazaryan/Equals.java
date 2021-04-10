fd2c280428a5c58f756d09705b7d9bfde44c78de
Narek-Ghazaryan <82145256+Narek-Ghazaryan@users.noreply.github.com>
2021-04-10 22:20:39 +0400

    Update Equals.java

--- Narek Ghazaryan/Equals.java
+++ Narek Ghazaryan/Equals.java
@@ -10,3 +10,26 @@ class Solution {
         }
     }
 }
+
+// AND VERY BAD VERSION
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+                String txt = "";
+                String txt2 = "";
+                for(int i = 0; i < word1.length; i++){
+                        txt += word1[i];
+                }
+                
+        
+                for(int j = 0; j < word2.length; j++){
+                        txt2 += word2[j];
+                }
+                
+                if(txt.equals(txt2)){
+                        return true;
+                }else {
+                        return false;
+                }
+
+    }
+}

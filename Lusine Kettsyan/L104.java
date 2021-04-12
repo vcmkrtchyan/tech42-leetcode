319efc223ea9e637a8bd5582f23d7682d4798da4
lucykettsyan <lucykettsyan@gmail.com>
2021-04-12 23:18:56 +0400

    L104

---
+++ Lusine Kettsyan/L104.java
@@ -0,0 +1,39 @@
+public class L104{
+
+     public static void main(String []args){
+         TreeNode n1 = new TreeNode(7);
+         TreeNode n2 = new TreeNode(15);
+         TreeNode n3 = new TreeNode(20, n1, n2);
+         TreeNode n4 = new TreeNode(3);
+         TreeNode n5 = new TreeNode(3,n4,n3);
+         System.out.println(maxDepth(n5));
+     }
+
+
+
+
+    public static int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftNode = maxDepth(root.left);
+        int  rightNode =  maxDepth(root.right);
+		
+        return Math.max(leftNode, rightNode) + 1;
+
+    }
+}
+
+    class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }

588f8c96f56b70bf2eac7de114cc4eccdd5f4613
goharvardanyan <gohar.vardanyanc@gmail.com>
2021-04-20 13:44:47 +0400

    Solution L590

--- Gohar Vardanyan/L504.java
+++ Gohar Vardanyan/L504.java
@@ -8,5 +8,24 @@ public class L504 {
             count *= 10;
         }
         return String.valueOf(result);
+
+        // long max = Long.MIN_VALUE;
+        // long max2 = Long.MIN_VALUE;
+        // long max3 = Long.MIN_VALUE;
+        // for(int i = 0; i < nums.length; i++) {
+        //     if(nums[i] > max) {
+        //         max3 = max2;
+        //         max2 = max;
+        //         max = nums[i];
+        //     }else if(nums[i] < max && nums[i] > max2) {
+        //         max3 = max2;
+        //         max2 = nums[i];
+        //     } else if(nums[i] < max2 && nums[i] > max3){
+        //         max3 = nums[i];
+        //     }
+        // }
+        // if(max3 == Long.MIN_VALUE) {
+        //     return (int) max;
+        // } else return (int) max3;
      }   
 }

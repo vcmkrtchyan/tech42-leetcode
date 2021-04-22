017d8aef7a2c5f3744a51ce7eec97359daa441ae
SatenHovakimyan <saten.hovakimyan5@gmail.com>
2021-04-22 14:29:28 +0400

    Saten L504

--- Saten Hovakimyan/L504.java
+++ Saten Hovakimyan/L504.java
@@ -9,4 +9,15 @@ public class L504 {
         }
         return String.valueOf(numBase7);
     }
+
+    // public static String convertToBase7(int num) {
+    // StringBuilder numBase7 = new StringBuilder();
+    // int numAbs = Math.abs(num);
+    // while (numAbs >= 7) {
+    // numBase7.append(numAbs % 7);
+    // numAbs /= 7;
+    // }
+    // return (num >= 0) ? numBase7.append(numAbs).reverse().toString() :
+    // numBase7.append(numAbs).append('-').reverse().toString();
+    // }
 }

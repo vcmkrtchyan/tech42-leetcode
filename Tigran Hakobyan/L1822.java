704ddf6f508e0e000526038b6bc1400b803d940b
tigranha <76497804+tigranha@users.noreply.github.com>
2021-04-26 15:25:28 +0400

    Update L1882

--- Tigran Hakobyan/L1822.java
+++ Tigran Hakobyan/L1822.java
@@ -1,23 +1,19 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int negCounter = 2;
+        int negCounter = 0;
         
         for (int i = 0; i < nums.length; i++){
             if (nums[i] == 0){
-                negCounter = 0;
-                break;
+                return 0;
             } else if (nums[i] < 0){
                 negCounter++;
             }
         }
         
-        if (negCounter == 0){
-            return 0;
-        } else if(negCounter%2 == 0){
-            negCounter = 1;
-        } else {
-            negCounter = -1;
-        }
-        return negCounter;
+       if (negCounter %2 == 0){
+           return 1;
+       } else {
+           return -1;
+       }
     }
 }

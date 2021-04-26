3673906687c8c7059c11b2a3aa4ca258bc80ba68
tigranha <76497804+tigranha@users.noreply.github.com>
2021-04-26 15:30:54 +0400

    better solution for L1882

--- Tigran Hakobyan/L1822.java
+++ Tigran Hakobyan/L1822.java
@@ -1,19 +1,14 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int negCounter = 0;
+        int result = 1;
         
         for (int i = 0; i < nums.length; i++){
-            if (nums[i] == 0){
+            if(nums[i] == 0){
                 return 0;
             } else if (nums[i] < 0){
-                negCounter++;
+                result = -result;
             }
-        }
-        
-       if (negCounter %2 == 0){
-           return 1;
-       } else {
-           return -1;
        }
+        return result;
     }
 }

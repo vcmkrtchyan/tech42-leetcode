139150a625dbe816c3a0181d3758af3a86733f17
tigranha <76497804+tigranha@users.noreply.github.com>
2021-05-14 15:36:12 +0400

    Update L709.java

--- L709.java
+++ L709.java
@@ -2,7 +2,7 @@ class Solution {
     public String toLowerCase(String str) {
       char[] strToCharArray = str.toCharArray();
         for (int i = 0; i < strToCharArray.length; i++)
-            if (64 < strToCharArray[i] && strToCharArray[i] < 97)
+            if (64 < strToCharArray[i] && strToCharArray[i] < 91)
                 strToCharArray[i] = (char) (strToCharArray[i] +32);
         return new String(strToCharArray);
     }

e4253acf255f3cf31a368a346d5de80b7755cb59
tigranha <76497804+tigranha@users.noreply.github.com>
2021-05-19 13:17:47 +0400

    Update L1689

--- Tigran Hakobyan/L1689.java
+++ Tigran Hakobyan/L1689.java
@@ -4,7 +4,26 @@ class Solution {
         for (int i = 0; i < n.length(); i++){
             if(num < (n.charAt(i) - '0')){
                 num = n.charAt(i) - '0';
+                if (num == 9){
+                    return num;
             }
+        }
+        return num;
+    }
+}
+    
+
+    
+    //////////////////////
+    
+    class Solution {
+    public int minPartitions(String n) {
+        int num = 0;
+        for (int i = 9; i >= 0; i--){
+           if (n.contains("" + i)){
+               num = i; 
+               return num;
+           }
         }
         return num;
     }

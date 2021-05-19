a042b84679a4c15bb576f102e4864c42a182150d
goharvardanyan <gohar.vardanyanc@gmail.com>
2021-05-19 16:41:30 +0400

    Update L1689

--- Gohar Vardanyan/L1689.java
+++ Gohar Vardanyan/L1689.java
@@ -1,10 +1,11 @@
 public class L1689 {
-     public int minPartitions(String n) {
-        int length = Math.min(n.length(), 10);
-        int max = n.charAt(0) - 48;
-        for (int i = 1; i < length; i++) {
-            if (n.charAt(i) - 48 > max) {
-                max = n.charAt(i) - 48;
+    public int minPartitions(String n) {
+        char[] chars = n.toCharArray();
+        int max = chars[0] - 48;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] - 48 > max) {
+                max = chars[i] - 48;
+                if (max == 9) return max;
             }
         }
         return max;

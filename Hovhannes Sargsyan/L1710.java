205e099311241b9bdd9c2f0a7c40c8e923f30d8c
Hovo9099 <Sargsyanhovo099@gmail.com>
2021-06-14 00:56:33 +0400

    Solved 1710

---
+++ Hovhannes Sargsyan/L1710.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        int [] arr = new int [1001];
+
+        for(int i =0; i < boxTypes.length; i++){
+            arr[boxTypes[i][1]] += boxTypes[i][0];
+        }
+
+
+        int maximum = 0, i = 1000;
+        while(i > 0) {
+            if(truckSize >= arr[i]) {
+                maximum += arr[i] * i;
+                truckSize -= arr[i];
+                i--;
+            }
+            else {
+                maximum += i * truckSize;
+                truckSize = 0;
+                break;
+            }
+        }
+
+        return maximum;
+    }
+}
